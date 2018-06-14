733ba1bb0c816a7ed9a63333b19670dda67bb516
sugeesh <sugeeshdeveloper@gmail.com>
2018-06-14 11:47:40 -0500

    handled errors

--- src/App.js
+++ src/App.js
@@ -1,32 +1,106 @@
-import React from "react";
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import FriendCard from "./components/FriendCard";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
 import friends from "./friends.json";
-import FriendList from "./friendlist";
+import "./App.css";
 
-const App = () => (
-  <Wrapper>
-    <Title>Friends List</Title>
-    <FriendCard
-      name={friends[0].name}
-      image={friends[0].image}
-      occupation={friends[0].occupation}
-      location={friends[0].location}
-    />
-    <FriendCard
-      name={friends[1].name}
-      image={friends[1].image}
-      occupation={friends[1].occupation}
-      location={friends[1].location}
-    />
-    <FriendCard
-      name={friends[2].name}
-      image={friends[2].image}
-      occupation={friends[2].occupation}
-      location={friends[2].location}
-    />
-  </Wrapper>
-);
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+class App extends Component {
+  // Set this.state
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Glaven!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Nav
+          title="Superb Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+
+        <Title>
+          Try to click on each character, but don't hit any duplicates, or
+          we'll release the hounds!!!
+        </Title>
+
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size="md-3 sm-6">
+                <FriendCard
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,23 +2,13 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card">
+  <div 
+    className="card" 
+    value={props.id} 
+    onClick={() => props.handleClick(props.id)}
+  >
     <div className="img-container">
       <img alt={props.name} src={props.image} />
-    </div>
-    <div className="content">
-      <ul>
-        <h1>{props.title}</h1>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
     </div>
   </div>
 );
